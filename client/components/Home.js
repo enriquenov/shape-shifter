a6be4dad04540ced4367301867391ab6b5dcc629
enrique novoa <luisenriquenovoa@gmail.com>
2019-01-17 12:31:07 -0800

    Feat: Rendered basic canvas and drew a square on it.

---
+++ client/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * COMPONENT
+ */
+const Home = () => {
+  return (
+    <div>
+      <h3>Welcome to Three.js by Enrique!</h3>
+      <p>Checkout the examples and have fun!</p>
+    </div>
+  )
+}
+
+export default Home
