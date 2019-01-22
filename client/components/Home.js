40c13150021e91a4620124d5d1a1b39738c5b209
enrique novoa <luisenriquenovoa@gmail.com>
2019-01-22 09:27:31 -0800

    Fixes.

--- client/components/Home.js
+++ client/components/Home.js
@@ -7,26 +7,27 @@ const Home = () => {
   return (
     <div className="container home">
       <h2>
-        ShapeShifter is a digital playground where you can interact with a few
-        3D Shapes (more to come) by manipulating their properties with the
-        purpose of having a visual understanding of how the basics of 3D
-        modeling works.
+        ShapeShifter is a digital playground where you can interact with 3D
+        Shapes and visualize the changes in real time by manipulating their
+        properties with the purpose of understanding a little how 3D modeling
+        works.
       </h2>
-      <br />
-      <br />
-      <p>* Spoiler alert *</p>
-      <p>Yes... it's pure math.</p>
       <br />
       <br />
       <hr />
       <br />
       <br />
-      <br />
-      <h3>( THREE.js + WebGL )</h3>
+      <h3>THREE.js</h3>
       <h3>&</h3>
-      <h3>( REACT.js / R3R )</h3>
-      <h3>=</h3>
+      <h3>REACT.js / R3R</h3>
+      <h3>V</h3>
       <h3>ShapeShifter</h3>
+      <br />
+      <br />
+      <p>* Spoiler alert *</p>
+      <p>
+        <em>Yes... at the end, it's pure math.</em>
+      </p>
     </div>
   )
 }
